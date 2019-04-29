2c6896314d192839978697ef65216680c69d15a0
biqing.hu <biqing.hu@zhaopin.com.cn>
2019-04-29 10:37:05 +0800

    ok

---
+++ src/koaFile.ts
@@ -0,0 +1,8 @@
+
+// 出处 https://segmentfault.com/q/1010000017848832/a-1020000017850816
+// koa2 转发文件上传接口
+// const request = require('request');
+// router.post('/upload', async (ctx) => {
+//   const res = await ctx.req.pipe(request.post(`https://example.com`)) // 这里请求真正的接口
+//   ctx.body = res
+// });
